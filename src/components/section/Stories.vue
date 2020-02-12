a1c0dcff4268391fa00d7fc520109fb885db4d52
Mustafa Ilyas <emailpribadiilyas@gmail>
2020-02-12 20:28:00 +0700

    add story card

--- src/components/section/Stories.vue
+++ src/components/section/Stories.vue
@@ -1,55 +1,59 @@
 <template>
     <section class="section-stories">
-                <div class="bg-video">
-                    <video class="bg-video__content" autoplay muted loop>
-                        <source :src="require('@/assets/img/video.mp4')" type="video/mp4">
-                        <source :src="require('@/assets/img/video.webm')" type="video/webm">
-                        Your browser is not supported!
-                    </video>
-                </div>
+        <div class="bg-video">
+            <video class="bg-video__content" autoplay muted loop>
+                <source :src="require('@/assets/img/video.mp4')" type="video/mp4">
+                <source :src="require('@/assets/img/video.webm')" type="video/webm">
+                Your browser is not supported!
+            </video>
+        </div>
 
-                <div class="u-center-text u-margin-bottom-big">
-                    <h2 class="heading-secondary">
-                        We make people genuinely happy
-                    </h2>
-                </div>
+        <div class="u-center-text u-margin-bottom-big">
+            <h2 class="heading-secondary">
+                We make people genuinely happy
+            </h2>
+        </div>
 
-                <div class="row">
-                    <div class="story">
-                        <figure class="story__shape">
-                            <img :src="require('@/assets/img/nat-8.jpg')" alt="Person on a tour" class="story__img">
-                            <figcaption class="story__caption">Mary Smith</figcaption>
-                        </figure>
-                        <div class="story__text">
-                            <h3 class="heading-tertiary u-margin-bottom-small">I had the best week ever with my family</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                                ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
-                                repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <StoryCard
+            v-for='(story, idx) in stories'
+            :key="idx"
+            :title="story.title"
+            :img="story.img"
+            :text="story.text"
+            :name="story.name"
+        />
 
-                <div class="row">
-                    <div class="story">
-                        <figure class="story__shape">
-                            <img :src="require('@/assets/img/nat-9.jpg')" alt="Person on a tour" class="story__img">
-                            <figcaption class="story__caption">Jack Wilson</figcaption>
-                        </figure>
-                        <div class="story__text">
-                            <h3 class="heading-tertiary u-margin-bottom-small">WOW! My life is completely different now</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                                ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
-                                repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="u-center-text u-margin-top-huge">
-                    <a href="#" class="btn-text">Read all stories &rarr;</a>
-                </div>
-            </section>
+        <div class="u-center-text u-margin-top-huge">
+            <a href="#" class="btn-text">Read all stories &rarr;</a>
+        </div>
+    </section>
 </template>
+
+<script>
+import StoryCard from '@/components/StoryCard'
+
+export default {
+  name: 'Story-Section',
+  components: {
+    StoryCard
+  },
+  data () {
+    return {
+      stories: [
+        {
+          img: 'nat-8.jpg',
+          name: 'Mary Smith',
+          title: 'I had the best week ever with my family',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.'
+        },
+        {
+          img: 'nat-9.jpg',
+          name: 'Jack Wilson',
+          title: 'WOW! My life is completely different now',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.'
+        }
+      ]
+    }
+  }
+}
+</script>
