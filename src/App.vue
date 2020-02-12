a9846c7fbd794c8907e28ae171ddb77e1c52220d
Mustafa Ilyas <emailpribadiilyas@gmail>
2020-02-12 17:54:01 +0700

    initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation/>
+    <!-- <Header/>
+    <Main/>
+    <Footer/>
+    <Popup/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
+// import Header from './components/Header.vue'
+// import Main from './components/Main.vue'
+// import Footer from './components/Footer.vue'
+// import Popup from './components/Popup.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
+    // Header,
+    // Main,
+    // Footer,
+    // Popup
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/sass/main";
 </style>
